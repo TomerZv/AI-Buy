d6166b492b779af42ba15a8510a10223c7ee5368
TomerZv <tomerzv@gmail.com>
2016-03-01 21:38:21 +0200

    commit

--- EbayAgentForm/EbayAgentForm/Form1.Designer.cs
+++ EbayAgentForm/EbayAgentForm/Form1.Designer.cs
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(125, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Find";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 227);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "EbayFinder";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 67);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(185, 154);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(209, 258);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- EbayAgentForm/EbayAgentForm/Form1.cs
+++ EbayAgentForm/EbayAgentForm/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Configuration;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using eBay.Services.Common;
+using eBay.Services.Finding;
+using eBay.Services.WcfExtension;
+using eBay.Service.Core;
+using eBay.Service.Core.Sdk;
+using eBay.Service.Core.Soap;
+using eBay.Service.Util;
+using eBay.Service.Call;
+using Slf;
+using eBay.Services;
+
 
 namespace EbayAgentForm
 {
     public partial class Form1 : Form
     {
+        private static ApiContext apiContext = null;
+        private FindingServicePortTypeClient client;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoggerService.SetLogger(new TraceLogger());
+
+            // Get AppID and ServerAddress from Web.config
+            string appID = System.Configuration.ConfigurationManager.AppSettings["AppID"];
+            string findingServerAddress = System.Configuration.ConfigurationManager.AppSettings["FindingServerAddress"];
+
+            ClientConfig config = new ClientConfig();
+            // Initialize service end-point configration
+            config.EndPointAddress = findingServerAddress;
+
+            // set eBay developer account AppID
+            config.ApplicationId = appID;
+
+            // Create a service client
+            client = FindingServiceClientFactory.getServiceClient(config);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create request object
+            FindItemsAdvancedRequest request = new FindItemsAdvancedRequest();
+
+            // Set request parameters
+            request.keywords = textBox1.Text;
+
+            if (request.keywords == null)
+            {
+                request.keywords = "ipod";
+            }
+            PaginationInput pi = new PaginationInput();
+            pi.entriesPerPage = 10;
+            pi.entriesPerPageSpecified = true;
+            request.paginationInput = pi;
+
+            // Call the service
+            FindItemsAdvancedResponse response = client.findItemsAdvanced(request);
+
+            if (response.searchResult != null && response.searchResult.item != null)
+            {
+                SearchItem[] items = response.searchResult.item;
+
+                foreach (SearchItem item in items)
+                {
+                    textBox2.Text += item.title + Environment.NewLine;
+                }
+            }
+
         }
     }
 }
