2c192f96f8547eb31eb1bdd047a6fbda0103047d
urielb92 <urielb92@gmail.com>
2016-04-27 17:42:26 +0300

    Fixed EF

---
+++ EbayAgentForm/AuctionManager/Models/Bid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AuctionManager.Models
+{
+    public class Bid 
+    {
+        [Key]
+        public int ID { get; set; }
+        public int AuctionID { get; set; }
+        public int Price { get; set; }
+        public string Username { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime Date { get; set; }
+    }
+}
